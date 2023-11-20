ac14e19039a32653c1dd4f5d3e0679c8250bbdf9
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-11-20 12:23:48 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { ThorinGlobalStyles, lightTheme } from '@ensdomains/thorin'
+import { Button, LockSVG } from '@ensdomains/thorin'
+import { Input, SearchSVG } from '@ensdomains/thorin'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { gql, useQuery } from '@apollo/client';
+const uri = 'https://api.studio.thegraph.com/query/1397/ens-delegatable-resolver-baseg/version/latest'
+
+const GET_APPROVALS = gql`
+  query getApprovals {
+    approvals(first:20) {
+      name
+      blockNumber
+      blockTimestamp
+    }
+  }
+`;
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri
+});
+const App = () => {
+  const { loading:queryLoading, error:queryError, data:queryData } = useQuery(GET_APPROVALS, {
+    client
+  });
+  const approvals = queryData?.approvals || []
+  console.log({approvals})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ApolloProvider client={client}>
+    <ThemeProvider theme={lightTheme}>
+      <ThorinGlobalStyles />
+      {/* {children} */}
+      <div style={{ width: '180px' }}>
+        <Button prefix={<LockSVG />} variant="primary">
+          Connect Wallet
+        </Button>
+      </div>
+      <div>
+        <Input
+          label="Register subname"
+          placeholder="nick"
+          suffix=".evmgateway.eth"
+          width='128'
+        />
+      </div>
+      <h3>Subname registrations</h3>
+      <ul>
+      {
+        approvals.map((approval)=> {
+          const blockTimestamp = approval.blockTimestamp
+          console.log(2, blockTimestamp)
+          const approvedAt = new Date(blockTimestamp * 1000)
+          console.log(3, approvedAt)
+          return (<li>
+            {approval.name} is registered at {approvedAt.toString()}
+          </li>)
+        })
+      }
+      </ul>
+    </ThemeProvider>
+    </ApolloProvider>
+  )
 }
-
 export default App;
