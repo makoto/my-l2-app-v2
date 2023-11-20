feb1770acc85f01f24206e7c3aa29f925ea34a11
Makoto Inoue <2630+makoto@users.noreply.github.com>
2023-11-20 13:09:28 +0100

    Show registered name on time order

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import { Button, LockSVG } from '@ensdomains/thorin'
 import { Input, SearchSVG } from '@ensdomains/thorin'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import { gql, useQuery } from '@apollo/client';
-const uri = 'https://api.studio.thegraph.com/query/1397/ens-delegatable-resolver-baseg/version/latest'
+import { Tag } from '@ensdomains/thorin'
+import moment from 'moment';
+
+const baseuri = 'https://api.studio.thegraph.com/query/1397/ens-delegatable-resolver-baseg/version/latest'
+const opuri = 'https://api.studio.thegraph.com/query/1397/ens-delegatable-resolver-opg/version/latest'
 
 const GET_APPROVALS = gql`
   query getApprovals {
@@ -19,14 +23,25 @@ const GET_APPROVALS = gql`
 `;
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri
+  uri: baseuri
+});
+const opclient = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: opuri
 });
 const App = () => {
-  const { loading:queryLoading, error:queryError, data:queryData } = useQuery(GET_APPROVALS, {
+  const { data:baseQueryData = [] } = useQuery(GET_APPROVALS, {
     client
   });
-  const approvals = queryData?.approvals || []
+  const { data:opQueryData = []} = useQuery(GET_APPROVALS, {
+    client: opclient
+  });
+  console.log({opQueryData,baseQueryData})
+  const opapprovals = (opQueryData?.approvals || []).map(a => { return {...a, ...{chain:'op'}} })
+  const baseapprovals = (baseQueryData?.approvals || []).map(a => { return {...a, ...{chain:'base'}} })
+  const approvals = [...baseapprovals,...opapprovals]
   console.log({approvals})
+
   return (
     <ApolloProvider client={client}>
     <ThemeProvider theme={lightTheme}>
@@ -48,13 +63,18 @@ const App = () => {
       <h3>Subname registrations</h3>
       <ul>
       {
-        approvals.map((approval)=> {
+        approvals.sort((a,b) => {return b.blockTimestamp - a.blockTimestamp} ).map((approval)=> {
           const blockTimestamp = approval.blockTimestamp
           console.log(2, blockTimestamp)
           const approvedAt = new Date(blockTimestamp * 1000)
           console.log(3, approvedAt)
-          return (<li>
-            {approval.name} is registered at {approvedAt.toString()}
+          return (<li style={{margin:"5px"}}>
+            {approval.chain === 'op' ? (
+              <Tag  style={{ display: 'inline', marginRight:"1em"   }} colorStyle="redPrimary">OP</Tag>
+            ):(
+              <Tag  style={{ display: 'inline', marginRight:"1em"   }} colorStyle="bluePrimary">Blue</Tag>
+            )}
+            {approval.name} is registered at {moment(approvedAt).format("YYYY-MM-DD hh:mm")}
           </li>)
         })
       }
