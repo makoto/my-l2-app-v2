ccbd4f6236f7032deea85adee73f416bde6373cf
Makoto Inoue <2630+makoto@users.noreply.github.com>
2024-02-26 12:39:22 +0000

    Add support for default reverse resolver

--- src/useL2PrimaryName.js
+++ src/useL2PrimaryName.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
-import { CHAIN_INFO,isL2 } from './utils'
+import { CHAIN_INFO, isL2, encodeName } from './utils'
+
 const abi = [
+  "function supportsInterface(bytes4) view returns(bool)",
   "function name(bytes32) view returns(string)",
   "function resolve(bytes,bytes) view returns (bytes)"
 ]
+const extendedResolverInterface = '0x9061b923'
+
 // Cannot use wagmi while  l2 gateway is hosted locally 
 // Because wagmi uses UniversalResolver that tries to hit gateway from CloudFlare and CF cannot reach localhost
 export default function useL2PrimaryName(provider, address, chain) {
@@ -15,11 +19,26 @@ export default function useL2PrimaryName(provider, address, chain) {
         const namespace       = `${coinType}.reverse`;
         const name            = address.substring(2).toLowerCase() + "." + namespace
         const reversenode     = ethers.namehash(name)
+        const encodedName     = encodeName(name)
         const resolver = new ethers.Contract(L1_REVERSE_RESOLVER_ADDRESS, abi, provider);
+        resolver.supportsInterface(extendedResolverInterface).then(r =>{
+          console.log('extendedResolverInterface1',{r})
+          if(r){
+            const i = new ethers.Interface(abi)
+            const calldata = i.encodeFunctionData("name", [reversenode])
+            resolver.resolve(encodedName, calldata, { enableCcipRead: true }).then(r => {
+              if(r){
+                setData(ethers.toUtf8String(r))
+              }
+            }).catch((e) =>{
+              console.log('useL2PrimaryName new error', e)
+            })    
+          }
+        })
         resolver.name(reversenode, { enableCcipRead: true }).then(r => {
           setData(r)
         }).catch((e) =>{
-          console.log('useL2PrimaryName error', e)
+          console.log('useL2PrimaryName old error', e)
         })
       }
   }, [address, chain?.id]);
